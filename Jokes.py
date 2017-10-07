58f79e74e298258c6ba8f247f549f65e054ddec5
natalia-guerrero <32421503+natalia-guerrero@users.noreply.github.com>
2017-10-07 10:24:07 -0700

    Add files via upload

---
+++ Jokes.py
@@ -0,0 +1,21 @@
+# Joke Teller
+# A game by Natalia Guerrero
+# Sept 30, 2017
+
+# This game simply tells jokes to the player.
+# Really its just an excuse for me to practice a few things
+# to prepare for our next big game
+
+# All jokes were stolen from the internet
+
+print("What kind of bagel can fly?")
+input()
+print("A plain bagel!")
+print("***HAHAHAHAHA***")
+print()
+
+print("Why don't they play poker in the jungle")
+input()
+print("Because there are too mant cheetahs")
+print("LOL!!")
+print()
